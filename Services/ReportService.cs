09c32f1d582055fe51eeaccc63a95332b70aa83a
Erdem Sincer <erdemsincer123@icloud.com>
2026-03-20 02:31:40 +0300

    refactor: ReportService OOP/SOLID prensiplerine uygun hale getirildi

--- Services/ReportService.cs
+++ Services/ReportService.cs
@@ -1,54 +1,132 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AIOOPAnalyzer.Services
 {
+    // --- Interfaces (Abstraction + DI) ---
+    public interface IReportRepository
+    {
+        void Save(Report report);
+        IReadOnlyList<Report> GetAll();
+    }
+
+    public interface IReportGenerator
+    {
+        Report Generate(string title);
+    }
+
+    public interface IReportNotifier
+    {
+        void Notify(string recipient, Report report);
+    }
+
+    // --- Model (Encapsulation) ---
+    public enum ReportType
+    {
+        Summary,
+        Detailed
+    }
+
+    public class Report
+    {
+        private readonly string _id;
+        private string _title;
+
+        public string Id => _id;
+        public string Title => _title;
+
+        public Report(string id, string title)
+        {
+            _id = id;
+            _title = title;
+        }
+
+        public void UpdateTitle(string newTitle)
+        {
+            _title = newTitle;
+        }
+    }
+
+    // --- Base class + Polymorphism ---
+    public abstract class BaseReportGenerator : IReportGenerator
+    {
+        protected virtual string FormatTitle(string title)
+        {
+            return $"[Rapor] {title}";
+        }
+
+        public Report Generate(string title)
+        {
+            var formatted = FormatTitle(title);
+            return new Report(Guid.NewGuid().ToString("N")[..8], formatted);
+        }
+    }
+
+    public class SummaryReportGenerator : BaseReportGenerator
+    {
+        protected override string FormatTitle(string title)
+        {
+            return $"[OZET] {title}";
+        }
+    }
+
+    public class DetailedReportGenerator : BaseReportGenerator
+    {
+        protected override string FormatTitle(string title)
+        {
+            return $"[DETAY] {title}";
+        }
+    }
+
+    // --- Repository (SRP) ---
+    public class InMemoryReportRepository : IReportRepository
+    {
+        private readonly List<Report> _reports = new();
+
+        public void Save(Report report)
+        {
+            _reports.Add(report);
+        }
+
+        public IReadOnlyList<Report> GetAll() => _reports.AsReadOnly();
+    }
+
+    // --- Notifier (SRP) ---
+    public class EmailReportNotifier : IReportNotifier
+    {
+        private readonly string _smtpHost;
+
+        public EmailReportNotifier(string smtpHost)
+        {
+            _smtpHost = smtpHost;
+        }
+
+        public void Notify(string recipient, Report report)
+        {
+            Console.WriteLine($"[EMAIL] '{report.Title}' -> {recipient} via {_smtpHost}");
+        }
+    }
+
+    // --- Service (DI ile bagimliliklari disaridan alir) ---
     public class ReportService
     {
-        public string dbConnection;
-        public string apiKey;
-        public List<string> reports;
+        private readonly IReportRepository _repository;
+        private readonly IReportGenerator _generator;
 
-        public void GenerateReport(string type)
+        public ReportService(IReportRepository repository, IReportGenerator generator)
         {
-            var db = new DatabaseConnector();
-            var data = db.GetData(type);
-            Console.WriteLine(data);
+            _repository = repository;
+            _generator = generator;
         }
 
-        public void SendReport(string to)
+        public Report CreateReport(string title)
         {
-            var mailer = new SmtpMailer();
-            mailer.Send(to, "Report", "data");
+            var report = _generator.Generate(title);
+            _repository.Save(report);
+            return report;
         }
 
-        public void SaveReport(string name)
-        {
-            Console.WriteLine($"Saving {name}");
-        }
-
-        public void DeleteReport(string id)
-        {
-            Console.WriteLine($"Deleting {id}");
-        }
-
-        public void ListReports()
-        {
-            foreach (var r in reports)
-                Console.WriteLine(r);
-        }
-    }
-
-    public class DatabaseConnector
-    {
-        public string connStr;
-        public string GetData(string query) { return "data"; }
-    }
-
-    public class SmtpMailer
-    {
-        public string host;
-        public int port;
-        public void Send(string to, string subject, string body) { }
+        public IReadOnlyList<Report> ListReports() => _repository.GetAll();
     }
 }
